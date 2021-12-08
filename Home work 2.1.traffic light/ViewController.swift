074f49719fd60dcbabdf546a1516845afda2a2a9
Dmitry <borodulkin_d@mail.ru>
2021-12-08 17:44:47 +0300

    Initial Commit

---
+++ Home work 2.1.traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2.1.traffic light
+//
+//  Created by Дмитрий Бородулькин on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
